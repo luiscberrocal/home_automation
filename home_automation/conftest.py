b480d82a77b9334ab227fde48200346684b9a055
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-10-02 18:58:55 -0500

    First commit

---
+++ home_automation/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from home_automation.users.models import User
+from home_automation.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
